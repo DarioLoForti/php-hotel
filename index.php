d4fc4ad72b7996ec54fe0ffcf1ee8be98c403348
DarioLoForti <lofortidario@gmail.com>
2024-01-25 14:51:52 +0100

    include file header in to index.php

--- index.php
+++ index.php
@@ -48,13 +48,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Hotel</title>
 </head>
 <body>
+    <?php include_once "./partials/header.php"; ?>
     <main>
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 py-4">
                     <table class="table table-striped">
                         <thead>
                             <tr>
--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,3 @@
 <header>
-    
+    <h1 class="text-center text-white py-4">Lista Hotel</h1>
 </header>
