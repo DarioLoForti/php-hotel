b64674a083168c4af3d7a6b8a5b63749376cf634
DarioLoForti <lofortidario@gmail.com>
2024-01-25 14:41:21 +0100

    create header and footer files

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
 </head>
 <body>
     <main>
-        <div class="contaainer">
+        <div class="container">
             <div class="row">
                 <div class="col-12">
                     <table class="table table-striped">
